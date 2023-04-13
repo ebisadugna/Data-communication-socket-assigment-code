eec69069e5bc828156c3c909630bfb3ebeae5ccc
ebisadugna <130274241+ebisadugna@users.noreply.github.com>
2023-04-14 01:30:47 +0300

    Add files via upload

---
+++ CLIENTthread.py
@@ -0,0 +1,38 @@
+from socket import *
+
+host = "10.5.220.130"
+port = 5050
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((host, port))
+
+# define variables
+message_error = "The number is out of range"
+message_verfication = " I received all messages thanks "
+client_name = "EBISA"  # to call the name of device
+
+# take input number from client
+client_number = input("Enter the number you choose : ")
+number_int = int(client_number)  # change client_number into integer
+
+while client_socket:
+    send_list = f"{client_name},{client_number},{message_verfication},{message_error}"
+    client_socket.send(send_list.encode())
+    recieve_message = client_socket.recv(1024).decode()
+    if (recieve_message == "The number is out of range"):
+        print(recieve_message)
+        client_socket.send(send_list[3].encode())
+        break
+    else:
+
+       # print(f"I am {client_name}")
+        recieve_list = recieve_message.split(",")
+        print(f"The client name is : {client_name}")
+        print(f"The server name is : {recieve_list[0]}")
+        print(f"The number choosen by client is : {number_int}")
+        print(f"The number choosen by the server is : {recieve_list[1]}")
+        print(
+            f"The sum of client & sever number is : {number_int + int(recieve_list[1])}")
+        client_socket.send(send_list[2].encode())
+        client_socket.close()
+        break
---
+++ SERVERthread.py
@@ -0,0 +1,39 @@
+from socket import *
+
+host = "0.0.0.0"
+port = 5050
+
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind((host, port))
+server_socket.listen()
+
+server_name = "YENADA"  # to call the name of the device
+print(f"{server_name} is listening ...")
+
+while True:
+    connection, address = server_socket.accept()
+    recieved_message = connection.recv(1024).decode()
+    recieve_list = recieved_message.split(",")
+
+    if (int(recieve_list[1]) <= 100 and int(recieve_list[1]) >= 0):
+
+        server_number = 77  # its random number of server
+        print(f"The server name is : {server_name}")
+        print(f"The client name is : {recieve_list[0]}")
+        print(f"The number choosen by the client is : {recieve_list[1]}")
+        print(f"The number choosen by server is : {server_number}")
+        print(
+            f"The sum of client & server number is : {server_number + int(recieve_list[1])}")
+
+        send_data = f"{server_name},{server_number}"
+        connection.send(send_data.encode())
+        print(f'verification from client : {recieve_list[2]}')
+        print(f'--------------------')
+        print("I am listening for other clients now....")
+        connection.close()
+    else:
+        send_data = "The number is out of range"
+        connection.send(send_data.encode())
+        print(recieve_list[3])
+        connection.close()
+        break
